c7d7c309a2f1e9a90245332bf8921f4245c6ad4c
jonathanaudu007 <jaudu2@gmail.com>
2022-09-27 16:57:17 +0100

    update

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
@@ -12,7 +13,25 @@ class PostController extends Controller
         $validator = Validator::make($req->all(),[
             'title' => 'string|max|20',
             'body' => 'required|string',
-
         ]);
+
+        if ($validator->fails()) {
+            $response = [
+                'status' => 'failure',
+                'status_code' => 500,
+                'errors' => $validator->errors(),
+            ];
+
+        return response()->json($response, 400);
+        }
+
+        $validator = $validator->validated();
+
+        $post = new Post;
+        $post->title = $validator['title'];
+        $post->body = $validator['body'];
+        $post->save();
+
     }
+
 }
